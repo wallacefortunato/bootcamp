4f47f706ccb8d1f5249d4fa02a35503ce9ba6e47
Wallace Fortunato <wallace.fortunato@engenharia.ufjf.br>
2023-03-15 02:14:06 -0300

    Atualizando projeto Estrutura de Dados - Aula 03 - Adicionando método de inserção de elementos no vetor

--- Java/Estrutura de Dados com Java/aula2-arrays/src/main/java/com/wallace/estruturadedados/vetor/Vetor.java
+++ Java/Estrutura de Dados com Java/aula2-arrays/src/main/java/com/wallace/estruturadedados/vetor/Vetor.java
@@ -8,4 +8,14 @@ public class Vetor {
     public Vetor (int capacidade){
         this.elementos = new String[capacidade];
     }
+
+    //Método para adicionar um novo elemento à primeira posição nula do vetor
+    public void adicionaElementoPrimeiraPosicaoNula(String element){
+        for (int i = 0; i < this.elementos.length; i++) {
+            if(this.elementos[i] == null){
+                this.elementos[i] = element;
+                break;
+            }
+        }
+    }
 }
---
+++ Java/Estrutura de Dados com Java/aula2-arrays/src/main/java/com/wallace/estruturadedados/vetor/teste/Aula03.java
@@ -0,0 +1,12 @@
+package com.wallace.estruturadedados.vetor.teste;
+
+import com.wallace.estruturadedados.vetor.Vetor;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        vetor.adicionaElementoPrimeiraPosicaoNula("Elemento 1");
+        vetor.adicionaElementoPrimeiraPosicaoNula("Elemento 2");
+    }
+}
