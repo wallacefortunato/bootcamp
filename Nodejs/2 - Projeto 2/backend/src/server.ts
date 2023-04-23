b6469f87e2d6480907b65fb1c948d662e2551f35
Wallace Fortunato <wallace.fortunato@engenharia.ufjf.br>
2023-04-23 18:22:08 -0300

    feat: adding debug on project

--- Nodejs/2 - Projeto 2/backend/src/server.ts
+++ Nodejs/2 - Projeto 2/backend/src/server.ts
@@ -4,6 +4,7 @@ import routes from "./routes"; //O routes se torna um middleware para o projeto.
 const app = express();
 const URL = "http://localhost:3333/";
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
